58980e937de92ece29d275528f16015f5efbd6f6
pedin777 <pedroaugusto878878788@gmail.com>
2023-11-02 13:45:10 -0300

    alteração na história

--- src/Livro.java
+++ src/Livro.java
@@ -1,62 +1,72 @@
 import java.util.Scanner;
-    class Livro_interativo {
-       public static void main(String[] args) throws Exception {
-           System.out.println("Oi,você pode fazer parte do livro interativo,\n"+
-           "me ajudando a construir uma historia.\n"+
-           "\nVamos lá?  - sim - nao\n");
-           Scanner start = new Scanner(System.in);
-           String init = start.nextLine();
-           if(init.equalsIgnoreCase("sim")){
-               Package livro_interativo;
-           } else if(init.equalsIgnoreCase("nao")){
-               System.out.println("\nBeleza,fica para uma proxima historia\n");
-               System.exit(0);
-           }
-   
-           Scanner response = new Scanner(System.in);
-               System.out.println("\n Vamos começar a construir a historia.\n");
-               System.out.println("\n\n"+
-               "      O que você vai fazer? (Digite sua escolha)\n\n"+
-               "-1   -2  * começou\n");
-               String choises = response.nextLine();
-               /* Primeira parte das escolhas*/
-               if(choises.equalsIgnoreCase("começou")){
-              System.out.println("\nVocê é um jogador de futebol ,\n"+
-              "\n e está em um jogo muito importante,\n"+
-              "\n você é o melhor jogador do time,\n"+
-              "\n o jogo esta 0x0 e já etá no segundo tempo e tem uma boa oportunidade ,\n"+
-              "\n o que você vai fazer?\n");
-   
-               } else if(choises.equalsIgnoreCase("1")){
-              System.out.println("\nVocê recebeu a bola e tem a opção de tocar,mais tambem pode continuar em direção ao gol");
-               System.out.println("\n\n"+
-               "      O que você vai fazer? (Digite sua escolha)\n\n"+
-                " *tocar  -Porque você tocou?\n");
-               // Respostas para parte 2
-                String choises2 = response.nextLine();
-              if(choises2.equalsIgnoreCase("Porque você tocou?")){
-                  System.out.println("\nPorque o outro jogador estava em condiçôes melhores\n"+
-                  "\n\n\n\nContinua\n");
-              }else if(choises2.equalsIgnoreCase("continuar com a bola")){
-                  System.out.println("\nVocê continua com a bola e dribla dois ,\n"+
-                 " \n corta por meio da área,chuta no canto do goleiro\n"+
-                  "\n e faz o gol do titulo\n\nFIM\n");
-              }
-   
-               }else if(choises.equalsIgnoreCase("2")){
-              System.out.println("\nVocê recebeu a bola ,partiu em velocidade ,\n"+
-              "\ndriblou 2 jogadores e o goleiro fez um penalti em você,");
-                System.out.println("\n\n"+
-               "      O que você vai fazer? (Digite sua escolha)\n\n"+
-               "-Vou bater o penalti\n");
-               String choises3 = response.nextLine();
-               if(choises3.equalsIgnoreCase("Vou bater o penalti")){
-              System.out.println("\nVocê pega a bola,posiciona bem na marca,pega distância ,se prepara ,,\n"+
-              "\ncorre para a bola e chuta no canto esquerdo do goleiro ,,\n"+
-              "\nconsegue marcar o gol,abre o placar ,,\n"+
-              "\nmais falta 15 minutos e pode acontecer mais coisas..\n"+
-              "\n\nCONTINUA");
-           }
-           }
-           }
-       }
+
+class Livro_interativo {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Oi,você pode fazer parte do livro interativo,\n" +
+                "me ajudando a construir uma historia.\n" +
+                "\nVamos lá?  - sim - nao\n");
+        Scanner start = new Scanner(System.in);
+        String init = start.nextLine();
+        if (init.equalsIgnoreCase("sim")) {
+            Package livro_interativo;
+        } else if (init.equalsIgnoreCase("nao")) {
+            System.out.println("\nBeleza,fica para uma proxima historia\n");
+            System.exit(0);
+        }
+
+        Scanner response = new Scanner(System.in);
+        System.out.println("\n Vamos começar a construir a historia.\n");
+        System.out.println("\nVocê é um jogador de futebol ,\n" +
+                "\n e está jogando uma final,\n" +
+                "\n você é o melhor jogador do time,\n" +
+                "\n o jogo está 0x0 e  tem uma falta \n" +
+                "\nperto da área para ser cobrada,\n");
+        System.out.println("\n\n" +
+                "      O que você vai fazer? (Digite sua escolha 1 ou 2)\n\n" +
+                "-1 Bater a falta    -2 Deixar seu companheiro bater \n");
+        String choises = response.nextLine();
+        /* Primeira parte das escolhas */
+        if (choises.equalsIgnoreCase("1")) {
+            System.out.println(
+                    "\nVocê pegou a bola ,ajeitou e bateu a falta\n" +
+                            "\n e fez o gol ,abrindo o placar.\n" +
+                            "\n Seu time segurou o resultado e acabou o jogo,\n" +
+                            "\n vocês foram campeões com seu gol de falta.\n");
+            System.out.println("Fim da Historia");
+
+        } else if (choises.equalsIgnoreCase("2")) {
+            System.out.println(
+                    "Seu companheiro bateu a falta\n" +
+                            "\nmais a bola foi na barreira \n" +
+                            "\ne no rebote ele chutou a bola para fora.\n");
+            System.out.println(
+                    "Nos acrescimos do segundo tempo com o jogo ainda 0x0\n" +
+                            "\n o zagueiro do outro time deu um carrinho \n" +
+                            "\n  no seu companheiro e fez um penalti\n" +
+                            "\n a ultima oportunidade para seu time fazer o gol.\n");
+            System.out.println("\n\n" +
+                    "      O que você vai fazer? (Digite sua escolha 1 ou 2)\n\n" +
+                    "-1 Bater o penalti    -2 Deixar seu companheiro bater \n");
+
+            String choises2 = response.nextLine();
+            if (choises2.equalsIgnoreCase("1")) {
+                System.out.println(
+                        "Você pegou a bola ,botou na marca do penalti\n" +
+                                "\n escolheu o canto e bateu ,mais o goleiro pegou\n" +
+                                "\n o jogo terminou empate ,foi para os penaltis\n" +
+                                "\n você fez o gol nos penaltis mais 2 de seus companheiros\n" +
+                                "\n perderam os penaltis e seu time não foi campeão.");
+                System.out.println("Fim da Historia");
+
+            } else if (choises2.equalsIgnoreCase("2")) {
+                System.out.println(
+                        "Seu companheiro bateu o penalti \n" +
+                                "\nbateu na direita,o goleiro ficou parado no meio\n" +
+                                "\ne foi gol.O jogo acabou 1x0 e seu time foi campeão.");
+                System.out.println("Fim da Historia");
+
+            }
+        }
+    }
+
+}
